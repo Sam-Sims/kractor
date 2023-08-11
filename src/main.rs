5d1ebb8c21ab95360151232d9ff72f332db1f443
Sam Sims <samsimss98@gmail.com>
2023-08-11 20:50:17 +0100

    tree struct, parsing of kraken report into tree

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,31 @@ use clap::Parser;
 use flate2::{read::GzDecoder, write::GzEncoder, Compression};
 use std::{
     collections::HashMap,
-    fs,
+    fs::{self, File},
     io::{self, prelude::*},
+    os::unix::process,
     sync::mpsc::channel,
     thread,
 };
+
+#[derive(Debug, Clone)]
+struct Tree {
+    taxon_id: i32,
+    level_num: i32,
+    children: Vec<usize>,
+    parent: Option<usize>,
+}
+
+impl Tree {
+    fn new(taxon_id: i32, level_num: i32, parent: Option<usize>) -> Tree {
+        Tree {
+            taxon_id,
+            level_num,
+            children: Vec::new(),
+            parent,
+        }
+    }
+}
 
 #[derive(Parser, Debug)]
 #[command(
@@ -18,35 +38,19 @@ struct Args {
     kraken: String,
     #[arg(short, long)]
     taxid: i32,
-    // #[arg(short, long)]
-    // report: String,
+    #[arg(short, long)]
+    report: Option<String>,
     #[arg(short, long)]
     fastq: String,
     #[arg(short, long)]
     output: String,
     #[arg(long, default_value = "default")]
     compression: Option<String>,
+    #[arg(long, action)]
+    parents: bool,
+    #[arg(long, action)]
+    children: bool,
 }
-
-// fn process_kraken_report(kraken_report: &str) -> (i32, &str, i32) {
-//     let fields: Vec<&str> = kraken_report.split('\t').collect();
-//     let taxon_id = fields[4].parse::<i32>().unwrap();
-//     let rank_code = fields[3];
-//     let mut spaces = 0;
-
-//     for char in fields[5].chars() {
-//         if char == ' ' {
-//             spaces = spaces + 1;
-//         }
-//         else {
-//             break;
-//         }
-//     }
-
-//     let level = spaces / 2;
-//     return (taxon_id, rank_code, level);
-
-// }
 
 fn process_kraken_output(kraken_output: &str) -> (i32, String) {
     let fields: Vec<&str> = kraken_output.split('\t').collect();
@@ -85,6 +89,76 @@ fn read_fastq(path: &str) -> io::BufReader<Box<dyn io::Read>> {
     }
 }
 
+fn process_kraken_report_line(kraken_report: &str) -> (i32, i32) {
+    let fields: Vec<&str> = kraken_report.split('\t').collect();
+    let taxon_id = fields[4].parse::<i32>().unwrap();
+    let mut spaces = 0;
+
+    for char in fields[5].chars() {
+        if char == ' ' {
+            spaces = spaces + 1;
+        } else {
+            break;
+        }
+    }
+
+    let level = spaces / 2;
+    (taxon_id, level)
+}
+
+fn process_kraken_report(taxon_to_save: i32, report_path: String) -> Vec<Tree> {
+    let mut nodes = Vec::new();
+    let mut taxon_to_index = HashMap::new();
+
+    let report_file = if let Ok(report_file) = fs::File::open(report_path) {
+        report_file
+    } else {
+        panic!("Error opening or reading the file");
+    };
+    {
+        let reader = io::BufReader::new(report_file);
+        let mut prev_index = None;
+
+        for line in reader.lines() {
+            if let Ok(line) = line {
+                let (taxon_id, level_num) = process_kraken_report_line(&line);
+                if taxon_id == 0 {
+                    continue;
+                }
+
+                if taxon_id == 1 {
+                    let root_node = Tree::new(taxon_id, level_num, None);
+                    prev_index = Some(nodes.len());
+                    nodes.push(root_node);
+                }
+
+                while let Some(parent_index) = prev_index {
+                    if level_num != nodes[parent_index].level_num + 1 {
+                        prev_index = nodes[parent_index].parent;
+                    } else {
+                        break;
+                    }
+                }
+
+                let curr_node = Tree::new(taxon_id, level_num, prev_index);
+                let curr_index = nodes.len();
+                nodes.push(curr_node);
+
+                if let Some(parent_index) = prev_index {
+                    nodes[parent_index].children.push(curr_index);
+                }
+
+                prev_index = Some(curr_index);
+
+                if taxon_id == taxon_to_save {
+                    taxon_to_index.insert(taxon_id, curr_index);
+                }
+            }
+        }
+    }
+    nodes
+}
+
 fn main() {
     let args = Args::parse();
 
@@ -97,16 +171,15 @@ fn main() {
             Compression::default()
         }
     };
+    // if --report is provided, read the report and build the tree
+    if args.report.is_some() {
+        println!("Reading kraken report");
+        let tree = process_kraken_report(args.taxid, args.report.unwrap());
 
-    // let kraken_report = fs::read_to_string(args.report)
-    //     .expect("Something went wrong reading the file");
+    }
 
-    // for line in kraken_report.lines() {
-    //     let line_values = process_kraken_report(line);
-    //     let (taxon_id, rank_code,mut level) = line_values;
-    // }
     let mut reads_to_save = HashMap::new();
-    println!("Reading kraken output...");
+    println!("Reading kraken output");
     let kraken_output = fs::read_to_string(args.kraken).expect("Error reading kraken output file");
 
     let total_reads = kraken_output.lines().count();
