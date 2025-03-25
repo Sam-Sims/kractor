ee7aded3f928dcc94b02a68899c2ebfee411e19d
Sam Sims <samsimss98@gmail.com>
2025-03-25 10:15:26 +0000

    feat: allow multiple taxids to be specified with -t (#19)
    * feat: allow multiple taxids to be specified with -t
    
    * fix: allow -t to specified multiple times for multiple taxids to extract

--- src/cli.rs
+++ src/cli.rs
@@ -26,8 +26,8 @@ pub struct Cli {
     #[arg(short = 'r', long = "report", value_parser(check_input_exists), required_if_eq_any([("parents", "true"), ("children", "true")]))]
     pub report: Option<PathBuf>,
     // Taxid to extract reads for
-    #[arg(short = 't', long = "taxid", required = true)]
-    pub taxid: i32,
+    #[arg(short = 't', long = "taxid", required = true, num_args(1..))]
+    pub taxid: Vec<i32>,
     // Compression type
     #[arg(
         short = 'O',
